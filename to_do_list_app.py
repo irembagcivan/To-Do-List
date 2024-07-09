21a9a7df77e49d829ab36c3f76aa6f0a45ef64eb
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-07-09 21:57:38 +0300

    add task

--- to_do_list_app.py
+++ to_do_list_app.py
@@ -1,8 +1,31 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from interface import Ui_mainWindow
 
 class ToDoList_interface(QtWidgets.QMainWindow, Ui_mainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # buton interaction
+        self.pushButton_addTask.clicked.connect(self.add_task)
+        self.pushButton_delete.clicked.connect(self.delete_completed_tasks)
+        self.pushButton_delete_all.clicked.connect(self.delete_all_tasks)
+    
+    def add_task(self):
+        task_text = self.lineEdit_tasks.text().strip()
+
+        if task_text:
+            item = QtWidgets.QListWidgetItem(task_text)
+            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            item.setCheckState(QtCore.Qt.Unchecked)
+            self.listWidget.addItem(item)
+            self.lineEdit_tasks.clear()
         
+        else:
+            QtWidgets.QMessageBox.warning(self, "Warning!", "Task cannot be empty!")
+
+    def delete_completed_tasks(self):
+        pass
+
+    def delete_all_tasks(self):
+        pass
